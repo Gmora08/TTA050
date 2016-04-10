717f64f4ee14416e4f182dc3c12e3d624ab05203
Gustavo Mora <gus@nubleer.com>
2016-04-10 16:07:51 -0500

    setup db

--- tt/settings.py
+++ tt/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'tt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tta050',
+        'USER': 'tta050',
+        'PASSWORD': 'tta050',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
+
 }
 
 
