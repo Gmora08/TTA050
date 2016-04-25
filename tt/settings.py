62b678610dfad1215f961c227ce43758f7d3146f
Gustavo Mora <gus@nubleer.com>
2016-04-25 03:26:51 -0500

    set templates and static routes

--- tt/settings.py
+++ tt/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'doctors',
+    'pacients',
+    'reports',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'tt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,7 +85,6 @@ DATABASES = {
         'PASSWORD': 'tta050',
         'HOST': 'localhost',
         'PORT': '',
-
     }
 
 }
@@ -125,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
